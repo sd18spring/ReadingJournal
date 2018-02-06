cee4ca69140a51f3b9ace60762b16bd311f111e6
Benjamin Hill <benjamin.hill@olin.edu>
2018-02-06 10:35:28 -0500

    Add day 5 reading journal exercises

---
+++ shapes.py
@@ -0,0 +1,10 @@
+import turtle
+
+# Create the world, and a turtle to put in it
+bob = turtle.Turtle()
+
+# Get moving, turtle!
+bob.fd(100)
+
+# Wait for the user to close the window
+turtle.mainloop()
